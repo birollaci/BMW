0e318994fea7b204787580f015c3089574a5417a
DESKTOP-UT1NVKC\Laci HP <birollaci@gmail.com>
2022-04-25 17:46:14 +0200

    first commit

---
+++ server/src/test/java/com/example/bmw/BmwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bmw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
